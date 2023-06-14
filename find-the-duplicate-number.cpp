731e94d1b7ec406a5d6282299a53bdf38c13b830
Prakashini83 <80676806+Prakashini83@users.noreply.github.com>
2023-06-14 12:11:20 +0530

    Create find-the-duplicate-number.cpp

---
+++ find-the-duplicate-number.cpp
@@ -0,0 +1,17 @@
+#https://leetcode.com/problems/find-the-duplicate-number/description/
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        map<int,int>m;
+        for(auto x:nums)
+        {
+            m[x]++;
+        }
+        for(auto x:m)
+        {
+            if(x.second>1)
+            return x.first;
+        }
+        return -1;
+    }
+};
