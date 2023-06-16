1e6568f6bfd71942a654881ec8d7afdc0376d019
Prakashini83 <80676806+Prakashini83@users.noreply.github.com>
2023-06-16 12:14:38 +0530

    Create decrypt-string-from-alphabet-to-integer-mapping.cpp

---
+++ decrypt-string-from-alphabet-to-integer-mapping.cpp
@@ -0,0 +1,18 @@
+#https://leetcode.com/problems/decrypt-string-from-alphabet-to-integer-mapping/description/
+class Solution {
+public:
+    string freqAlphabets(string s) {
+        string ans="";
+        for(int i=s.size()-1; i>=0; i--){
+            if(s[i]=='#'){
+                ans+=(((s[--i]-'0')+(s[--i]-'0')*10)-1)+'a';
+                //i++;
+            }
+            else{
+                ans+=(s[i]-'0')-1+'a';
+            }
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
