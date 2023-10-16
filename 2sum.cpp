ba4b1319b5ff2633a0baa92d697078ec1f9f284d
Prakashini83 <80676806+Prakashini83@users.noreply.github.com>
2023-10-16 23:42:07 +0530

    Update 2sum.cpp

--- 2sum.cpp
+++ 2sum.cpp
@@ -1,4 +1,4 @@
-#https://leetcode.com/problems/two-sum/description/
+# https://leetcode.com/problems/two-sum/description/
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
